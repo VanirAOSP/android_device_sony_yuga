d7dc1975d220607c3e92f325ff11746808fcc003
DHOMD <DHO@dho.im>
2014-02-12 20:11:43 -0500

    Try 4.7 toolchain, new cortex based linaro toochains do not include 4.6
    Change-Id: Ife788e6764c91cd405aa8e4226a43a5ea51e632e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,8 +15,6 @@
 # inherit from the common fusion3 definitions
 include device/sony/fusion3-common/BoardConfigCommon.mk
 
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.6
-
 # inherit from the proprietary version
 -include vendor/sony/yuga/BoardConfigVendor.mk
 
